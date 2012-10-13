a575334b71fd6fd90647692be871f606cb63f85e
Michael George <mdgeorge@cs.cornell.edu>
2012-10-13 17:48:50 -0400

    added definitions for Transitive, NoZeroDivisors

--- src/com/mdgeorge/algebra/properties/NoZeroDividers.java
+++ src/com/mdgeorge/algebra/properties/NoZeroDividers.java
@@ -1,7 +1,22 @@
 package com.mdgeorge.algebra.properties;
 
+import com.mdgeorge.algebra.properties.meta.Binary;
 import com.mdgeorge.algebra.properties.meta.MagicProperty;
+import com.mdgeorge.algebra.properties.meta.Nullary;
 
 @MagicProperty
 public @interface NoZeroDividers {
+	
+	public static class Definition {
+		public static <E>
+		boolean check ( Binary<E,E,E> m
+		              , Binary<E,E,Boolean> eq, Nullary<E> zero
+		              , E a, E b
+		              )
+		{
+			return eq.ap(m.ap(a,b), zero.ap())
+			     ? eq.ap(a, zero.ap()) || eq.ap(b, zero.ap())
+			     : true;
+		}
+	}
 }
--- src/com/mdgeorge/algebra/properties/Transitive.java
+++ src/com/mdgeorge/algebra/properties/Transitive.java
@@ -2,6 +2,7 @@ package com.mdgeorge.algebra.properties;
 
 import java.lang.annotation.*;
 
+import com.mdgeorge.algebra.properties.meta.Binary;
 import com.mdgeorge.algebra.properties.meta.MagicProperty;
 
 @Retention(RetentionPolicy.RUNTIME)
@@ -9,4 +10,14 @@ import com.mdgeorge.algebra.properties.meta.MagicProperty;
 @Documented
 @MagicProperty
 public @interface Transitive {
+
+	public static class Definition {
+		public static <E>
+		boolean check ( Binary<E,E,Boolean> m
+		              , E a, E b, E c
+		              )
+		{
+			return m.ap(a, b) && m.ap(b,c) ? m.ap(a, c) : true;
+		}
+	}
 }
