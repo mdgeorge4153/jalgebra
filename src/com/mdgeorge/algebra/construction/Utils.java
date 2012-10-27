5653e5d509fe51bbd2ae9d58fa31f8dde21c2abc
Michael George <mdgeorge@cs.cornell.edu>
2012-10-26 20:25:28 -0400

    using binary instead of unary construction

--- src/com/mdgeorge/algebra/construction/Utils.java
+++ src/com/mdgeorge/algebra/construction/Utils.java
@@ -9,9 +9,12 @@ public class Utils {
 	 */
 	public static <E, R extends Ring<E>> E intInRing(int n, R r) {
 		E result = r.zero();
-		for (int i = 0; i < n; i++)
-			result = r.plus(result, r.one());
 		
+		E twoToTheI = r.one();
+		for (int i = 0; n >> i == 0; i++, twoToTheI = r.plus(twoToTheI, twoToTheI))
+			if ((i & 0x1) == 1)
+				result = r.plus(result, twoToTheI);
+
 		return result;
 	}
 }
