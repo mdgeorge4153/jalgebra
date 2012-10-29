02b1c641d1de49d8aa9080c70aad05302a23c591
Michael George <mdgeorge@cs.cornell.edu>
2012-10-28 21:34:40 -0400

    Preorder homs

---
+++ src/com/mdgeorge/algebra/concept/PreorderHom.java
@@ -0,0 +1,12 @@
+package com.mdgeorge.algebra.concept;
+
+import com.mdgeorge.algebra.properties.Preserves;
+
+public interface PreorderHom < DE, D extends Preorder<DE>
+                                 , CE, C extends Preorder<CE>
+                                 >
+         extends SetHom<DE, D, CE, C>
+{
+	@Preserves("Preorder.leq")
+	CE ap (DE e);
+}
