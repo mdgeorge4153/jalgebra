260ef607d8cb70a5858dbb68257c676d6c17fd1e
Michael George <mdgeorge@cs.cornell.edu>
2012-11-17 21:43:31 -0500

    poked at arrangements

--- src/com/mdgeorge/geometry/Arrangement.java
+++ src/com/mdgeorge/geometry/Arrangement.java
@@ -1,5 +1,25 @@
 package com.mdgeorge.geometry;
 
-public class Arrangement {
+import java.util.List;
 
+public class Arrangement {
+	private static class Face {
+		Halfedge             boundary;
+		List<IsolatedVertex> interior;
+	}
+	
+	private static class Vertex {
+		Halfedge leaving;
+	}
+	
+	private static class Halfedge       {
+		Vertex   origin;
+		Halfedge next;
+		Halfedge twin;
+		Face     face;
+	}
+	
+	private static class IsolatedVertex {
+		Face face;
+	}
 }
