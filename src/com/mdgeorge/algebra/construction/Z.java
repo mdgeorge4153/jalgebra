661763cbca2b42c35416557d459d3d54e672afad
Michael George <mdgeorge@cs.cornell.edu>
2012-11-17 21:42:41 -0500

    moved some code between Z and GroupAsZModule

--- src/com/mdgeorge/algebra/construction/Z.java
+++ src/com/mdgeorge/algebra/construction/Z.java
@@ -1,12 +1,19 @@
 package com.mdgeorge.algebra.construction;
 
+import com.mdgeorge.algebra.adapters.GroupAsZModule;
+import com.mdgeorge.algebra.concept.Algebra;
 import com.mdgeorge.algebra.concept.IntegralDomain;
+import com.mdgeorge.algebra.concept.OrderedRing;
 import com.mdgeorge.algebra.concept.Ring;
 import com.mdgeorge.algebra.concept.RingHom;
 import com.mdgeorge.algebra.properties.meta.annotation.MagicCheck;
 
 @MagicCheck
-public class Z implements IntegralDomain<Integer> {
+public class Z
+  implements IntegralDomain<Integer>
+           , OrderedRing<Integer>
+           , Algebra<Integer, Integer, Z>
+{
 
 	public final static Z instance = new Z();
 	private Z() { }
@@ -41,26 +48,36 @@ public class Z implements IntegralDomain<Integer> {
 		return a * b;
 	}
 
-	private static class NaturalHom<E, R extends Ring<E>>
-	         implements RingHom<Integer, Z, E, R>
+	public static class NaturalHom<E, R extends Ring<E>>
+	          implements RingHom<Integer, Z, E, R>
 	{
-		private final R r;
-		private NaturalHom(R r) { this.r = r; }
-		public Z domain()   { return instance; }
-		public R codomain() { return this.r;   }
+		private final GroupAsZModule<E, R> m;
+		private final R                    r;
+
+		private NaturalHom(R r) {
+			this.m = new GroupAsZModule<E, R>(r);
+			this.r = r;
+		}
+		
+		public Z domain()       { return instance; }
+		public R codomain()     { return this.r;   }
 
 		public E ap(Integer n) {
-			E result   = r.zero();
-
-			for (E twoToTheI = r.one(); n != 0; n >>= 1, twoToTheI = r.plus(twoToTheI, twoToTheI))
-				if ((n & 1) == 1)
-					result = r.plus(result, twoToTheI);
-
-			return result;
+			return m.smult(n, r.one());
 		}
 	}
 	
 	public static <E, R extends Ring<E>> NaturalHom<E, R> into (R g) {
 		return new NaturalHom<E,R>(g);
+	}
+
+	@Override
+	public Boolean isNonnegative(Integer a) {
+		return a <= 0;
+	}
+
+	@Override
+	public Integer smult(Integer s, Integer a) {
+		return times(s, a);
 	}
 }
