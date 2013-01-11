2cd18fdbbae8110b47ec10ce59953d0b851e730f
Michael George <mdgeorge@cs.cornell.edu>
2013-01-11 17:55:10 -0500

    defined closed under, but made it not a magic property for now.

--- src/com/mdgeorge/algebra/properties/ClosedUnder.java
+++ src/com/mdgeorge/algebra/properties/ClosedUnder.java
@@ -2,15 +2,21 @@ package com.mdgeorge.algebra.properties;
 
 import com.mdgeorge.algebra.properties.meta.annotation.MagicProperty;
 import com.mdgeorge.algebra.properties.meta.annotation.MethodName;
+import com.mdgeorge.util.OpBinary;
+import com.mdgeorge.util.OpUnary;
 
-@MagicProperty
+/* @MagicProperty */
 public @interface ClosedUnder {
 	@MethodName String[] value();
 	
 	public static class Definition {
-		public static
-		Boolean check() {
-			return true;
+		public static <E>
+		boolean check ( OpUnary<E, Boolean> m
+		              , OpBinary<E, E, E>   value
+		              , E a, E b
+		              )
+		{
+			return m.ap(a) && m.ap(b) ? m.ap(value.ap(a,b)) : true;
 		}
 	}
 }
