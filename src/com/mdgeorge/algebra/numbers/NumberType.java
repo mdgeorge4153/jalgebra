25d5cfa92316749ebe1821c7dc1d66a997984695
Michael George <mdgeorge@cs.cornell.edu>
2016-09-25 21:49:32 -0400

    made constructor protected

--- src/com/mdgeorge/algebra/numbers/NumberType.java
+++ src/com/mdgeorge/algebra/numbers/NumberType.java
@@ -3,5 +3,5 @@ package com.mdgeorge.algebra.numbers;
 public abstract class NumberType {
 	public final int value;
 
-	NumberType(int n) { this.value = n; }
+	protected NumberType(int n) { this.value = n; }
 }
