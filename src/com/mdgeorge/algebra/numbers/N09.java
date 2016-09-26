9b629418b431fa1fcd4ffb61e943da0b9bab810b
Michael George <mdgeorge@cs.cornell.edu>
2016-09-25 21:49:44 -0400

    more numbers!

---
+++ src/com/mdgeorge/algebra/numbers/N09.java
@@ -0,0 +1,6 @@
+package com.mdgeorge.algebra.numbers;
+
+public final class N09 extends NumberType {
+	public static final N09 instance = new N09();
+	private N09() { super(9); }
+}
---
+++ src/com/mdgeorge/algebra/numbers/N10.java
@@ -0,0 +1,6 @@
+package com.mdgeorge.algebra.numbers;
+
+public final class N10 extends NumberType {
+	public static final N10 instance = new N10();
+	private N10() { super(10); }
+}
